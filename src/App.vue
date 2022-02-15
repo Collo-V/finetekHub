4834b8566972e6818ece60c3ddc8262f91696283
Collo_V <collinsvictoromondi@gmail.com>
2022-02-15 11:27:50 +0300

    First

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Slide from './components/Slide.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Slide
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Slide.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="slide">
+    <div class="slide slide1 h-500px w-ful">
+      Lorem ipsum.
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.slide1{background: linear-gradient(to right, #0e0d0da9,#0e0d0da9), url(../assets/Images/Slide/planning2.jpg) no-repeat center/cover;};
+
+</style>
