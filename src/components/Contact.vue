6659ca18d641ae94453e50ff1691afa86793743d
Collo-V <collinsvictoromondi@gmail.com>
2022-03-08 12:55:21 +0300

    Contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,45 +5,68 @@
         <h2 class="text-36px">Contact me</h2>
         <form action="" class="mt-8">
           <fieldset class="h-36px w-full relative mb-3">
-            <input class="form-input pl-4 w-full h-full flex items-center rounded-sm focus:outline-none border-1px">
+            <input class="form-input pl-4 w-full h-full flex items-center rounded-sm focus:outline-none border-1px"
+            id="firstName" @blur="Validate($event.target.id)">
             <div class="form-label h-0 w-full absolute pl-4 top-1/2 btm-1/2 flex items-center">
               <label class="bg-white px-1">First Name*</label>
-              <span class="validity-checker"></span>
+            </div>
+            <div class="validity-checker h-0 w-full absolute pr-2 top-1/2 btm-1/2 flex items-center justify-end">
+              <span class="hidden valid text-green"><i class="fa-solid fa-check"></i></span>
+              <span class="hidden invalid text-red"><i class="fa-solid fa-triangle-exclamation"></i></span>
             </div>
           </fieldset>
           <fieldset class="h-36px w-full relative mb-3">
-            <input class="form-input pl-4 w-full h-full flex items-center rounded-sm focus:outline-none border-1px">
+            <input class="form-input pl-4 w-full h-full flex items-center rounded-sm focus:outline-none border-1px"
+            id="lastName" @blur="Validate($event.target.id)">
             <div class="form-label h-0 w-full absolute pl-4 top-1/2 btm-1/2 flex items-center">
               <label class="bg-white px-1">Last Name*</label>
-              <span class="validity-checker"></span>
+            </div>
+            <div class="validity-checker h-0 w-full absolute pr-2 top-1/2 btm-1/2 flex items-center justify-end">
+              <span class="hidden valid text-green"><i class="fa-solid fa-check"></i></span>
+              <span class="hidden invalid text-red"><i class="fa-solid fa-triangle-exclamation"></i></span>
             </div>
           </fieldset>
           <fieldset class="h-36px w-full relative mb-3">
-            <input class="form-input pl-4 w-full h-full flex items-center rounded-sm focus:outline-none border-1px">
+            <input class="form-input pl-4 w-full h-full flex items-center rounded-sm focus:outline-none border-1px"
+            id="email" @blur="Validate($event.target.id)">
             <div class="form-label h-0 w-full absolute pl-4 top-1/2 btm-1/2 flex items-center">
               <label class="bg-white px-1">Email*</label>
-              <span class="validity-checker"></span>
+            </div>
+            <div class="validity-checker h-0 w-full absolute pr-2 top-1/2 btm-1/2 flex items-center justify-end">
+              <span class="hidden valid text-green"><i class="fa-solid fa-check"></i></span>
+              <span class="hidden invalid text-red"><i class="fa-solid fa-triangle-exclamation"></i></span>
             </div>
           </fieldset>
           <fieldset class="h-36px w-full relative mb-3">
-            <input class="form-input pl-4 w-full h-full flex items-center rounded-sm focus:outline-none border-1px">
+            <input class="form-input pl-4 w-full h-full flex items-center rounded-sm focus:outline-none border-1px"
+            id="phone" @blur="Validate($event.target.id)">
             <div class="form-label h-0 w-full absolute pl-4 top-1/2 btm-1/2 flex items-center">
               <label class="bg-white px-1">First Name*</label>
-              <span class="validity-checker"></span>
+            </div>
+            <div class="validity-checker h-0 w-full absolute pr-2 top-1/2 btm-1/2 flex items-center justify-end">
+              <span class="hidden valid text-green"><i class="fa-solid fa-check"></i></span>
+              <span class="hidden invalid text-red"><i class="fa-solid fa-triangle-exclamation"></i></span>
             </div>
           </fieldset>
           <fieldset class="h-36px w-full relative mb-3">
-            <input class="form-input pl-4 w-full h-full flex items-center rounded-sm focus:outline-none border-1px">
+            <input class="form-input pl-4 w-full h-full flex items-center rounded-sm focus:outline-none border-1px"
+            id="company" @blur="Validate($event.target.id)">
             <div class="form-label h-0 w-full absolute pl-4 top-1/2 btm-1/2 flex items-center">
               <label class="bg-white px-1">Company</label>
-              <span class="validity-checker"></span>
+            </div>
+            <div class="validity-checker h-0 w-full absolute pr-2 top-1/2 btm-1/2 flex items-center justify-end">
+              <span class="hidden valid text-green"><i class="fa-solid fa-check"></i></span>
+              <span class="hidden invalid text-red"><i class="fa-solid fa-triangle-exclamation"></i></span>
             </div>
           </fieldset>
           <fieldset class="w-full relative mb-3">
             <textarea class="form-input pl-4 w-full h-24 flex items-center rounded-sm focus:outline-none border-1px"></textarea>
             <div class="form-label h-0 w-full absolute pl-4 top-1/2 btm-1/2 flex items-center">
               <label class="bg-white px-1">How can we help you?*</label>
-              <span class="validity-checker"></span>
+            </div>
+            <div class="validity-checker h-0 w-full absolute pr-2 top-1/2 btm-1/2 flex items-center justify-end">
+              <span class="hidden valid text-green"><i class="fa-solid fa-check"></i></span>
+              <span class="hidden invalid text-red"><i class="fa-solid fa-triangle-exclamation"></i></span>
             </div>
           </fieldset>
           <div class="text-grey-dark">
@@ -69,7 +92,7 @@
           <h2 class="text-center">What happens next</h2>
           <div class="mt-3">
             <div class="flex mb-8">
-              <div class="w-1/4 min-w-8 mr-3">
+              <div class="min-w-8 mr-3">
                 <div class="next-circle h-8 w-8 flex items-center justify-center rounded-full">1</div>
               </div>
               <div>
@@ -78,7 +101,7 @@
               </div>
             </div>
             <div class="flex mb-8">
-              <div class="w-1/4 min-w-8 mr-3">
+              <div class="min-w-8 mr-3">
                 <div class="next-circle h-8 w-8 flex items-center justify-center rounded-full">2</div>
               </div>
               <div>
@@ -86,7 +109,7 @@
               </div>
             </div>
             <div class="flex mb-8">
-              <div class="w-1/4 min-w-8 mr-3">
+              <div class="min-w-8 mr-3">
                 <div class="next-circle h-8 w-8 flex items-center justify-center rounded-full">3</div>
               </div>
               <div>
@@ -103,13 +126,78 @@
 
 <script>
 export default {
-  name: "Contact"
+  name: "Contact",
+  data(){},
+  methods:{
+    Validate(id){
+      let inp = document.getElementById(id)
+      let val = inp.value
+      if(id='FirstName'){
+        if(val=='' || val.length<2){
+          inp.classList.add('input-invalid')
+          inp.classList.remove('input-valid')
+          return
+        }
+      }  
+      else if(id=='lastName'){
+        if(val=='' || val.length<2){
+          inp.classList.add('input-invalid')
+          inp.classList.remove('input-valid')
+          return
+        }
+      }
+      else if(id=='email'){
+        if(
+          !val.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+        ){
+          inp.classList.add('input-invalid')
+          inp.classList.remove('input-valid')
+          return
+        }
+      }
+      else if(id=='company'){
+        if(val=='' || val.length<2){
+          inp.classList.add('input-invalid')
+          inp.classList.remove('input-valid')
+          return
+        }
+      }
+      else if(id=='phone'){
+        if(val=='' || val.length<2){
+          inp.classList.add('input-invalid')
+          inp.classList.remove('input-valid')
+          return
+        }
+      }
+      else{
+        if(val=='' || val.length<2){
+          inp.classList.add('input-invalid')
+          inp.classList.remove('input-valid')
+          return
+        }
+      }
+      inp.classList.add('input-valid')
+      inp.classList.remove('input-invalid')
+      console.log(inp.classList)
+      function ValidateEmail(mail) {
+        console.log("here")
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+          {
+            return (true)
+          }
+        return (false)
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
 .contact{background: url(~@/assets/images/contact-form-background.jpg) no-repeat top}
 .form-label{transform: translateY(-50%)}
-.form-input:focus + .form-label{top: 0;color: #1476f2;font-size: smaller}
+.form-input:focus + .form-label,.input-valid + .form-label,
+.input-invalid + .form-label{top: 0;color: #1476f2;font-size: smaller}
 .next-circle{background: #1865c7;}
+.input-invalid~.validity-checker .invalid{display: initial;}
+.input-valid~.validity-checker .valid{display: initial;}
 </style>
