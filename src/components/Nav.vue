e7da9f431f3f309335f501d04d84819c3c7f3e41
Collo-V <collinsvictoromondi@gmail.com>
2023-03-01 23:35:56 +0300

    Add sweet clock

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,7 +3,7 @@
    <div class="fixed top-0 left-0 w-full h-16 shadow-md flex justify-between bg-grey-light z-10">
      <div class="h-full w-200px bg-primary-purple"></div>
      <div class="flex flex-row h-full items-center pr-3 bg-grey-light gap-3">
-       <div v-html="Time">
+       <div v-html="time">
 
        </div>
        <div class="relative dropdown-cont">
@@ -37,10 +37,25 @@
       </router-link>
 
    </div>
+    <div class="mt-4 flex justify-center ">
+      <div class="clock">
+        <div class="hour">
+          <div class="hr" id="hr"></div>
+        </div>
+        <div class="min">
+          <div class="mn" id="mn"></div>
+        </div>
+        <div class="sec">
+          <div class="sc" id="sc"></div>
+        </div>
+      </div>
+    </div>
   </div>
+  <span class="hidden">{{clock}}</span>
   <div class="lg:hidden">
 <!--    <MobNav/>-->
   </div>
+
 
 </template>
 
@@ -82,7 +97,7 @@ export default {
     //   let newChats = state.chats.newChats
     //   return state.chats.newChats
     // },
-    Time(state){
+    time(state){
       return dateFormatter(state.time,'long-slash')
     },
     GetNavs({user}){
@@ -97,10 +112,25 @@ export default {
         return this.mainNavs
       }
 
+    },
+    clock(state) {
+      let date = new Date(state.time);
+      console.log('here')
+      const deg = 6;
+      const hr = document.querySelector('#hr');
+      const mn = document.querySelector('#mn');
+      const sc = document.querySelector('#sc');
+      let hh = date.getHours() * 30;
+      let mm = date.getMinutes() * deg;
+      let ss = date.getSeconds() * deg;
+      hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+      mn.style.transform = `rotateZ(${mm}deg)`;
+      sc.style.transform = `rotateZ(${ss}deg)`;
+      return date
     }
-
   }),
   mounted() {
+    this.clock
     // this.user = this.$router.getters.GetUser
   }
 }
@@ -109,6 +139,93 @@ export default {
 <style scoped>
 .router-link-active{
   @apply text-primary-yellow bg-primary-purple-light
+}
+
+.clock
+{
+  width: 150px;
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: url(../assets/images/icons/clock.webp);
+  background-size: cover;
+  border: 4px solid #262162;
+  border-radius: 50%;
+  box-shadow: 0 -15px 15px rgba(255,255,255,0.05),
+  inset 0 -15px 15px rgba(255,255,255,0.05),
+  0 15px 15px #262162,
+  inset 0 15px 15px #262162;
+}
+
+.clock::before
+{
+  content: '';
+  position: absolute;
+  width: 15px;
+  height: 15px;
+  background: #fff;
+  border-radius: 50%;
+  z-index: 10000;
+}
+.clock .hour,
+.clock .min,
+.clock .sec
+{
+  position: absolute;
+}
+.clock .hour, .hr
+{
+  width: 60px;
+  height: 80px;
+}
+.clock .min, .mn
+{
+  width: 100px;
+  height: 100px;
+}
+.clock .sec, .sc
+{
+  width: 120px;
+  height: 120px;
+}
+.hr, .mn, .sc
+{
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+  position: absolute;
+  border-radius: 50%;
+}
+.hr:before
+{
+  content: '';
+  position: absolute;
+  width: 8px;
+  height: 50px;
+  background: #ff105e;
+  z-index: 10;
+  border-radius: 6px 6px 0 0;
+}
+.mn:before
+{
+  content: '';
+  position: absolute;
+  width: 4px;
+  height: 60px;
+  background: #fff;
+  z-index: 11;
+  border-radius: 6px 6px 0 0;
+}
+.sc:before
+{
+  content: '';
+  position: absolute;
+  width: 2px;
+  height: 80px;
+  background: #fff;
+  z-index: 12;
+  border-radius: 6px 6px 0 0;
 }
 
 </style>
