39d8d33b78858df80f2eb5e91212e8da933c2a03
Collo-V <collinsvictoromondi@gmail.com>
2023-03-03 13:21:18 +0300

    Remove user select, remove broken links from netlify

--- src/App.vue
+++ src/App.vue
@@ -111,7 +111,7 @@ export default {
 <style>
     button{color: inherit}
 * {
-  font-family: 'Montserrat', sans-serif;box-sizing: border-box;user-select: none;
+  font-family: 'Montserrat', sans-serif;box-sizing: border-box;
 }
  a{color: inherit;text-decoration: none}
 button:focus,button:active{outline: none;}
